d2c858bcc96da55f3126b0c76f943f8a5c185d86
nagappan <nagappan@shortcastle.com>
2026-09-06 18:07:45 +0800

    Mark ScreenCaptureKit as preconcurrency for Swift 6 CI.
    GitHub's Xcode 16.4 SDK treats SCShareableContent as non-Sendable across the async excludingDesktopWindows boundary; the local SDK does not.

--- Sources/Layer/ScreenContextCapture.swift
+++ Sources/Layer/ScreenContextCapture.swift
@@ -1,7 +1,7 @@
 import AppKit
 import CoreGraphics
 import Foundation
-import ScreenCaptureKit
+@preconcurrency import ScreenCaptureKit
 
 struct ScreenAttachment: Equatable, Sendable {
     let imageData: Data
